1397160b84dc2c08a3cd183357ef3db25cba600a
Aaandria <andriabegiashvili5@gmail.com>
2025-05-03 10:49:15 +0400

    third commit

---
+++ day 03/homework/homework 03.py
@@ -0,0 +1,16 @@
+name = 'andria'
+surname = "begiashvili"
+age = "14"
+
+print ("hello, my name is " + name  + " and my surname is " + surname + ", " + "I am " + age + " years old")
+
+number1 = 10
+number2 = 24
+number3 = 22
+number4 = 4
+number5 = 100
+
+number5 / number1
+number4 * number3
+number2 + number5
+number2 - number3
