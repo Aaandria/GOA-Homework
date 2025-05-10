7ec1879751297f67007cf8d212cec47517b2711f
Aaandria <andriabegiashvili5@gmail.com>
2025-05-10 12:38:17 +0400

    fifth commit

---
+++ day 05/homework/honework05.py
@@ -0,0 +1,27 @@
+number = input()
+
+if number > 10 :
+    print("you are right ")
+
+num1 = input()
+num2 = input()
+
+num1 + num2
+num1 - num2
+num1 * num2
+num1 / num2
+num1 % num2
+num1 < num2
+num1 > num2
+num1 <= num2
+num1 >= num2
+num1 == num2
+num1 != num2
+num1 ** num2
+
+#if შედგება 2 ნაწილისგან თავი და სხეული. თავში მხოლოდ if და bool იწერება და ის ოველთვის ერთ ხაზზე იწერება,
+#ხოლო სხეულში უკვე იწერება მოქმედებები და ის თავის ქვედა ხაზებში იწერება რამდენიმე სფეისით
+#მაგალითად : 
+# if True :
+#    print("hello")
+#თუ ბულიანი არის True მაშინ კომპიუტერი if-ს კითხულობს, ხოლო თუ False მაშინ ტოვებს.
