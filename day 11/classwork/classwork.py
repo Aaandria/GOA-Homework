33afce88e2445e6394ba11dfad7292642b2760c7
Aaandria <andriabegiashvili5@gmail.com>
2025-06-07 22:46:44 +0400

    twelveth commit

--- day 11/classwork/classwork.py
+++ day 11/classwork/classwork.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 num1 = int(input("enter a number "))
 num2 = int(input("enter second number "))
 num3 = int(input("enter third number "))
 sum = num1 + num2 + num3
 
 print(sum)
-=======
-num1 = int(input("enter a number "))
-num2 = int(input("enter second number "))
-num3 = int(input("enter third number "))
-sum = num1 + num2 + num3
-
-print(sum)
->>>>>>> 184f5995a20ef554b0ebaa86a71dee56e261b512
