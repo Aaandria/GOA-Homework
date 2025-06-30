7292715a533b7431da3103fb3e1a08bdc61e0cc2
Aaandria <andriabegiashvili5@gmail.com>
2025-06-30 13:43:17 +0400

    eightinghts commit

---
+++ day 19/classwork/classwork00.py
@@ -0,0 +1,15 @@
+# ინდექსინგი გვეხმარება string-ის ან list-ის რომელიმე წევრის გამოტანაში ან შეცვლაში
+
+lst = [20, "abc", 10.05, True, "hello world"]
+
+print(lst[0])
+print(lst[1])
+print(lst[2])
+print(lst[3])
+print(lst[4])
+
+lst[0] = 10
+lst[1] = "zyx"
+lst[2] = 5.25
+lst[3] = False
+lst[4] = "world hello"
---
+++ day 19/homework/homework02.py
@@ -0,0 +1,8 @@
+# 3:
+#სია:
+
+words = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape']
+
+#აიღე სიის ბოლო 2 ელემენტი slicing-ის მეშვეობით.
+
+print(words[4 : ])
