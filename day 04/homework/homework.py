f1415db139b92694a172cba76f7aaaf83b599150
Aaandria <andriabegiashvili5@gmail.com>
2025-06-30 15:13:39 +0400

    fourth commit

--- day 04/homework/homework.py
+++ day 04/homework/homework.py
@@ -15,8 +15,8 @@ print(P)
 #ინფუთის მეშვეობით მომხმარებელი წერს თავის ინფორმაციას
 #აუთფუთს გამოაქვს ინფორმაცია
 
-number1 = input()
-number2 = input()
+number1 = int(input())
+number2 = int(input())
 
 print(number1 + number2)
 print(number1 - number2)
