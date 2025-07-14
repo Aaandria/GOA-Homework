dd2f9c0bea8f5ee6250c2478cc65162748271128
Aaandria <andriabegiashvili5@gmail.com>
2025-07-14 13:58:30 +0400

    twentinght commit

---
+++ day 21/homework/homework03.py
@@ -0,0 +1,7 @@
+#დაბეჭდე სტრინგი "Hello, World!" უკუღმა, ყოველი მეორე ასო.
+
+text = "Hello, World!"
+
+#შედეგი უნდა იყოს: '!lo olH'
+
+print(text[-1] + text[-3] + text[-5] + text[-7] + text[-9] + text[-11] + text[-13])
